a8972b701652f44ffd2618839ec4bdc04fed724e
Hieu Tran <hieutran.fu@gmail.com>
2020-11-26 23:32:09 +0700

    fix: add seach input handler

--- simpos/src/apps/pos/components/SearchPanel/SearchPanel.tsx
+++ simpos/src/apps/pos/components/SearchPanel/SearchPanel.tsx
@@ -8,17 +8,32 @@ import {
 } from '@chakra-ui/react';
 import React from 'react';
 import { IconSearch } from '../../../../components/icons/output/IconSearch';
+import {
+  useSearchProductDispatch,
+  useSearchProductState,
+} from '../../../../contexts/SearchProduct';
 
-export const SearchPanel: React.FunctionComponent = () => (
-  <Flex align="center" px={4} mb={2}>
-    <Box mr="auto">
-      <Heading size="md">Danh sach san pham</Heading>
-    </Box>
-    <InputGroup size="md" width="250px">
-      <Input pr="3rem" placeholder="Tim kiem" />
-      <InputRightElement width="3rem">
-        <IconSearch size="20" color="gray" />
-      </InputRightElement>
-    </InputGroup>
-  </Flex>
-);
+export const SearchPanel: React.FunctionComponent = () => {
+  const state = useSearchProductState();
+  const dispatch = useSearchProductDispatch();
+  return (
+    <Flex align="center" px={4} mb={2}>
+      <Box mr="auto">
+        <Heading size="md">Danh sách sản phẩm</Heading>
+      </Box>
+      <InputGroup size="md" width="250px">
+        <Input
+          pr="3rem"
+          placeholder="Tìm kiếm"
+          value={state.keyword}
+          onChange={(event) =>
+            dispatch({ type: 'KEYWORD_CHANGED', payload: event.target.value })
+          }
+        />
+        <InputRightElement width="3rem">
+          <IconSearch size="20" color="gray" />
+        </InputRightElement>
+      </InputGroup>
+    </Flex>
+  );
+};
