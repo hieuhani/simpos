12df39e3f1c7cbbe5c654638b15a3dd99045534e
Hieu Tran <hieutran.fu@gmail.com>
2020-12-18 22:47:52 +0700

    feat: add sample service worker

--- simpos/src/index.tsx
+++ simpos/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 import { AuthProvider } from './contexts/AuthProvider';
 import { Routes } from './configs/routes';
 import { ThemeProvider } from './configs/themes';
@@ -23,3 +25,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
