3bc4bbfb8a867d88984f622c55570c0896dea460
Hieu Tran <hieutran.fu@gmail.com>
2020-12-22 10:24:02 +0700

    update baumkuchen theme

--- simpos_addons/baumkuchen/__manifest__.py
+++ simpos_addons/baumkuchen/__manifest__.py
@@ -13,7 +13,7 @@
     'website': "https://youngtailors.com",
     'category': 'Uncategorized',
     'version': '0.1',
-    'depends': ['polaco', 'simpos', 'simpos_vibration_card', 'simpos_customer_display'],
+    'depends': ['polaco', 'simpos', 'simpos_vibration_card', 'simpos_customer_display_baumkuchen'],
     'qweb': [
         'static/src/xml/*.xml',
     ],
