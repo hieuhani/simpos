e240de265212bbcf68c97b868f72e3a95804078c
Hieu Tran <hieutran.fu@gmail.com>
2021-01-06 23:16:45 +0700

    feat: add purchase view

--- simpos/src/apps/purchase/Purchase.tsx
+++ simpos/src/apps/purchase/Purchase.tsx
@@ -1,4 +1,43 @@
+import { Box, Container, Flex, Link } from '@chakra-ui/react';
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
 import { NavigationBarGeneral } from '../pos/components/NavigationBar';
 
-export const Purchase: React.FunctionComponent = () => <NavigationBarGeneral />;
+export const Purchase: React.FunctionComponent = () => (
+  <>
+    <NavigationBarGeneral />
+    <Container maxW="6xl" pt={4}>
+      <Flex>
+        <Link to="/purchase" as={RouterLink}>
+          <Box
+            backgroundColor="pink.100"
+            borderRadius="full"
+            py={2}
+            px={4}
+            color="pink.700"
+            fontWeight="600"
+          >
+            Mua hàng
+          </Box>
+        </Link>
+        <Link
+          to={{ pathname: '/purchase', search: '?status=waiting' }}
+          as={RouterLink}
+        >
+          <Box py={2} px={4} color="gray.600" fontWeight="600">
+            Chờ nhận hàng
+          </Box>
+        </Link>
+        <Link
+          to={{ pathname: '/purchase', search: '?status=received' }}
+          as={RouterLink}
+        >
+          <Box py={2} px={4} color="gray.600" fontWeight="600">
+            Đã nhận hàng
+          </Box>
+        </Link>
+      </Flex>
+    </Container>
+  </>
+);
