8e84c127d5f14fb382bd2d14c659713659f024ed
Hieu Tran <hieutran.fu@gmail.com>
2021-02-16 18:46:15 +0700

    feat: create so from po

--- simpos_addons/simpos_sync_po_from_so/__manifest__.py
+++ simpos_addons/simpos_sync_po_from_so/__manifest__.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Generate Sale Order from Purchase Order',
+    'name': 'Create Sale Order from Purchase Order',
     'version': '2.1',
     'category': 'Tools',
     'description': """
 Generate Sale Order from Purchase Order
 ========================
     """,
-    'depends': ['purchase'],
-    'data': [
-        'views/res_partner_views.xml',
-    ],
+    'depends': ['sale'],
+    'data': [],
     'installable': True,
 }
